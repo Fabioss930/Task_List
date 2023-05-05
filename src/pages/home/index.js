3da6b386aadf169565997b50fee71a782264ea13
Fabio Silva <38165103+Fabioss930@users.noreply.github.com>
2023-05-04 23:52:21 -0400

    Adicionando Drawer na Home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,50 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import IconUser from "@mui/icons-material/Person";
+import { ComponentToolBar, ComponentUser } from "./styles";
 
 const Home = () => {
-  return(
-    
-    <div>Home</div>
-    
-    
-  )
-}
+  const drawerWidth = 240;
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: `calc(100% - ${drawerWidth}px)`,
+          ml: `${drawerWidth}px`,
+          backgroundColor: "#FFFFFF",
+        }}
+      >
+        <Toolbar>
+          <ComponentToolBar>
+            <ComponentUser>
+              <div style={{ marginRight: 18 }}>Fabio</div>
+              <IconUser />
+            </ComponentUser>
+          </ComponentToolBar>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            backgroundColor: "#121620",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      ></Drawer>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/pages/home/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ComponentToolBar = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  color: black;
+`;
+
+export const ComponentUser = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
