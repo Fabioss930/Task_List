08a0ff0850f5b9f03503617c3d6ebd6c18c0ec23
Yagosbk <yagosbk@hotmail.com>
2023-05-07 20:55:30 -0400

    Ajuste das telas login e registro
    Adicionados os logos e a coloração da tela.

--- src/pages/Signup/styles.js
+++ src/pages/Signup/styles.js
@@ -1,14 +1,49 @@
 import styled from "styled-components";
 
 
+
+export const Page = styled.div`
+
+ position: absolute;
+ width: 773px;
+ height: 713px;
+ left: 0px;
+ top: 0px;
+ 
+ background: linear-gradient(0deg, #1A202E, #1A202E), url(london.jpg);
+ `;
+
+ export const Image = styled.div`
+ position: absolute;
+ width: 464px;
+ height: 349px;
+ left: 154px;
+ top: 160px;
+ 
+ 
+ `;
+
+
+ export const Image2 = styled.div`
+ position: absolute;
+ width: 200px;
+ height: 200px;
+ left: 1000px;
+ top: 74px; 
+ 
+ `;
+
 export const Container = styled.div`
 
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;
-gap: 10px;
-height: 100vh;
+padding-left: 60vw;
+ padding-right: 0vw;
+ padding-top: 45vh;
+ padding-bottom: 9vh;
+ justify-content: center;
+ flex-direction: column;
+ gap: 10px;
+ width: 1300px;
+ height: 100vh;
 `;
 
 export const Content = styled.div`
@@ -29,9 +64,16 @@ border-radius: 5px;
 
 export const Label = styled.label`
 
- font-size: 18px;
- font-weight: 600;
- color: #676767;
+position: absolute;
+  width: 400px;
+  height: 230px;
+  left: 950px;
+  top: 180px;
+  font-size: 40px;
+  font-weight: 600;
+  color: #000000;;
+  display: flex;
+  align-items: center;
 
 
 `;
