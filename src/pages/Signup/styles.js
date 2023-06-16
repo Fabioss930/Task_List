e822fbcfae3c403c36b1a7c779a331f048c22075
Fabio Silva <38165103+Fabioss930@users.noreply.github.com>
2023-06-16 19:13:43 -0400

    responsividade na page de criar conta

--- src/pages/Signup/styles.js
+++ src/pages/Signup/styles.js
@@ -14,6 +14,10 @@ export const ContentLogo = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 976px) {
+    display: none;
+  }
 `;
 
 export const ContentForm = styled.div`
@@ -23,6 +27,10 @@ export const ContentForm = styled.div`
   flex-direction: column;
   height: 100vh;
   width: 50vw;
+
+  @media (max-width: 976px) {
+    width: 100%;
+  }
 `;
 
 export const Form = styled.div`
@@ -32,27 +40,24 @@ export const Form = styled.div`
   flex-direction: column;
 `;
 
-
 export const Image = styled.div`
- width: 464px;
- height: 349px;
- `;
-
-export const Image2 = styled.div`
-  max-width: 130px;
   width: 100%;
-  
-  
+  text-align: center;
 `;
 
-export const Label = styled.label`
+export const Image2 = styled.div``;
 
+export const Label = styled.label`
   font-style: normal;
   font-weight: 700;
   font-size: 39px;
   line-height: 48px;
   display: flex;
   align-items: center;
+
+  @media (max-width: 526px) {
+    font-size: 35px;
+  }
 `;
 
 export const LabelImput = styled.label`
@@ -63,14 +68,21 @@ export const LabelImput = styled.label`
   color: #696f79;
   width: 100%;
   padding: 14px 0;
+
+  @media (max-width: 526px) {
+    font-size: 14px;
+  }
 `;
 
 export const Entry1 = styled.div`
   box-sizing: border-box;
   width: 426px;
-  
   border: 1px solid #8692a6;
   border-radius: 6px;
+
+  @media (max-width: 526px) {
+    width: 100%;
+  }
 `;
 
 export const LabelError = styled.label`
